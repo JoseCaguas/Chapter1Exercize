7f28183589dfa2f41f6bda6fff094c48e56af59c
jlt26922 <jlt26922@HT-0232-19>
2025-01-17 10:30:47 -0500

    Add project files.

---
+++ Chapter1Exercize.cpp
@@ -0,0 +1,19 @@
+/*
+File Name: Chapter1Exercize1.cpp
+Programer: Jose L. Torres
+Date: Jpan 17, 2025
+Requirements: 
+Sum of two Numbers 50 and 100. Put in variables.
+Add number and place in varriables and display resuls.
+*/
+
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+    cout << "Hello World!\n";
+    return 0;
+}
